70ec9f44dc3487603221b88b7514b8d60249b144
AZch <antonzhuikov@gmail.com>
2021-09-24 09:33:04 +0300

    remove unused code

--- src/main/java/com/wcreators/exchangeratesjava/ExchangeRatesJavaApplication.java
+++ src/main/java/com/wcreators/exchangeratesjava/ExchangeRatesJavaApplication.java
@@ -14,22 +14,7 @@ import java.util.List;
 public class ExchangeRatesJavaApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(ExchangeRatesJavaApplication.class, args);
-        List<Integer> data = new LinkedList<>();
-        data.add(0);
-        data.add(1);
-        data.add(2);
-        data.add(3);
-        data.add(4);
-        data.add(5);
-        data.add(6);
-        data.forEach(System.out::println);
-        System.out.println();
-        System.out.println(data.size());
-        if (data.size() > 6) {
-            data.subList(0, 3).clear();
-        }
-        data.forEach(System.out::println);
+        SpringApplication.run(ExchangeRatesJavaApplication.class, args);
     }
 
 }
